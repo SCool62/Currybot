21019e0b5d7b81f3b0983b3088159b6239478329
SCool62 <samthecool300@icloud.com>
2025-11-20 22:12:30 -0800

    Add IO interface and sim implementation

---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.elevator;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ElevatorIO {
+    @AutoLog
+    public class ElevatorIOInputs {
+        public double leaderPositionMeters = 0.0;
+        public double leaderVelocityMetersPerSec = 0.0;
+        public double leaderStatorCurrentAmps = 0.0;
+        public double leaderSupplyCurrentAmps = 0.0;
+        public double leaderVoltage = 0.0;
+        public double leaderTempC = 0.0;
+
+        public double followerPositionMeters = 0.0;
+        public double followerVelocityMetersPerSec = 0.0;
+        public double followerStatorCurrentAmps = 0.0;
+        public double followerSupplyCurrentAmps = 0.0;
+        public double followerVoltage = 0.0;
+        public double followerTempC = 0.0;
+    }
+
+    void updateInputs(ElevatorIOInputs inputs);
+
+    void setPositionSetpoint(double positionMeters);
+
+    void setVoltage(double voltage);
+
+    void setEncoderPosition(double positionMeters);
+}
---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+
+public class ElevatorIOSim implements ElevatorIO {
+    // Technically x44 but DCmotor doesn't have it
+    // TODO: ACTUAL VALUES
+    private ElevatorSim physicsSim = new ElevatorSim(
+        DCMotor.getKrakenX60Foc(2), 
+        0,
+        0, 
+        0, 
+        0, 
+        0, 
+        true, 
+        0, 
+        null
+    );
+
+    // TODO: TUNE IN SIM
+    private ElevatorFeedforward feedforward = new ElevatorFeedforward(0, 0, 0);
+    private ProfiledPIDController pid = new ProfiledPIDController(0, 0, 0, new Constraints(5.0, 10.0));
+
+    private double currentAppliedVoltage = 0.0;
+
+    public ElevatorIOSim() {}
+
+    @Override
+    public void updateInputs(ElevatorIOInputs inputs) {
+        inputs.leaderPositionMeters = physicsSim.getPositionMeters();
+        inputs.leaderVoltage = currentAppliedVoltage;
+        inputs.leaderStatorCurrentAmps = physicsSim.getCurrentDrawAmps();
+        inputs.leaderVelocityMetersPerSec = physicsSim.getVelocityMetersPerSecond();
+
+        inputs.followerPositionMeters = inputs.leaderPositionMeters;
+        inputs.followerVoltage = inputs.leaderVoltage;
+        inputs.followerStatorCurrentAmps = inputs.leaderStatorCurrentAmps;
+        inputs.followerVelocityMetersPerSec = inputs.leaderVelocityMetersPerSec;
+    }
+
+    @Override
+    public void setVoltage(double voltage) {
+        physicsSim.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void setPositionSetpoint(double positionMeters) {
+        setVoltage(pid.calculate(physicsSim.getPositionMeters(), positionMeters) + feedforward.calculate(pid.getSetpoint().velocity));
+    }
+
+    @Override
+    public void setEncoderPosition(double positionMeters) {
+        physicsSim.setState(positionMeters, 0.0);
+    }
+    
+}
